cba6d62155318564b45a452f99dbee0f5cc6ec8c
CatAvadani <catalinaavadani@gmail.com>
2024-04-27 14:03:01 +0200

    Initial setup with tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,36 @@
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
+
+// function App() {
+//   return (
+//     <div className=' flex flex-col min-h-screen'>
+//       <header className=' flex justify-between bg-sky-100 p-4 font-extrabold'>
+//         <Link to='/'>Home</Link>
+//         <Link to='/login'>Login</Link>
+//       </header>
+//       <main>
+//         <Outlet />
+//       </main>
+//       <footer>Footer</footer>
+//     </div>
+//   );
+// }
+
+export default App;
 
 function App() {
   return (
-    <>
-      <h1>Welcome to the Event App</h1>
-    </>
+    <div className='flex flex-col h-screen'>
+      <header className='bg-gray-100 p-4 font-extrabold text-xl'>
+        <div className='flex justify-between max-w-7xl mx-auto'>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+        </div>
+      </header>
+      <main className='flex-grow flex items-center justify-center'>
+        <Outlet />
+      </main>
+      <footer className='bg-gray-100 p-4 text-center'>Footer</footer>
+    </div>
   );
 }
-
-export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import App from './App';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' Component={App}>
+      <Route index Component={HomePage} />
+      <Route path='login' Component={LoginPage} />
+      <Route path='*' element={<div>Page not found</div>} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
