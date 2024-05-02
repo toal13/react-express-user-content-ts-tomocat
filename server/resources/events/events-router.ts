f19eed9a6fc3a79f8867b477852768e31d6e942b
CatAvadani <catalinaavadani@gmail.com>
2024-05-02 17:45:02 +0200

    Checking is user is logged in when create, update, delete event.

--- server/resources/events/events-router.ts
+++ server/resources/events/events-router.ts
@@ -1,18 +1,19 @@
-import express from "express";
+import express from 'express';
+import { isLoggedIn } from '../users/users-middlewares/isLoggedIn-middleware';
 import {
   createEvent,
   deleteEvent,
   getAllEvents,
   getEvent,
   updateEvent,
-} from "./events-handler";
+} from './events-handler';
 
 export const eventsRouter = express.Router();
 
-eventsRouter.get("/", getAllEvents);
-eventsRouter.get("/:id", getEvent);
-eventsRouter.post("/", createEvent);
-eventsRouter.put("/:id", updateEvent);
-eventsRouter.delete("/:id", deleteEvent);
+eventsRouter.get('/', getAllEvents);
+eventsRouter.get('/:id', getEvent);
+eventsRouter.post('/', isLoggedIn, createEvent);
+eventsRouter.put('/:id', isLoggedIn, updateEvent);
+eventsRouter.delete('/:id', isLoggedIn, deleteEvent);
 
 export default eventsRouter;
