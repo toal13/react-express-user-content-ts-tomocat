49930cd04b6199ad4e49416dcfd8f6b607a57562
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-05-04 11:02:44 +0200

    add save button function

--- client/src/pages/CreateEvent.tsx
+++ client/src/pages/CreateEvent.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 export default function CreateEvent() {
+  const [title, setTitle] = useState("");
+  const [place, setPlace] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSave = (event) => {
+    event.preventDefault();
+    const formData = {
+      title,
+      place,
+      date,
+      time,
+      image,
+      description
+    };
+
+    console.log('saved event:', formData);
+  };
+
   return (
     <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
       <h1 className="text-xl font-bold text-white capitalize dark:text-white">
         Create event
       </h1>
-      <form>
+      <form onSubmit={handleSave} method="POST">
         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
           <div>
             <label className="text-white dark:text-gray-200" for="username">
@@ -13,17 +36,21 @@ export default function CreateEvent() {
             <input
               id="title"
               type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             />
           </div>
 
           <div>
             <label className="text-white dark:text-gray-200" for="emailAddress">
-              Adress
+              Address
             </label>
             <input
-              id="place"
+              id="address"
               type="text"
+              value={place}
+              onChange={(e) => setPlace(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             />
           </div>
@@ -37,6 +64,8 @@ export default function CreateEvent() {
             <input
               id="date"
               type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             />
           </div>
@@ -50,6 +79,8 @@ export default function CreateEvent() {
             <input
               id="time"
               type="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             />
           </div>
@@ -58,8 +89,10 @@ export default function CreateEvent() {
               URL
             </label>
             <input
-              id="password"
-              type="password"
+              id="image"
+              type="text"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             />
           </div>
@@ -71,15 +104,20 @@ export default function CreateEvent() {
               Description
             </label>
             <textarea
-              id="content"
-              type="textarea"
+              id="description"
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             ></textarea>
           </div>
         </div>
 
         <div className="flex justify-end mt-6">
-          <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
+          <button
+            type="submit"
+            className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600"
+          >
             Save
           </button>
         </div>
