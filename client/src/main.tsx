4a3a1c0ca868f0c994e367c8bf64ccd3a180a5d1
CatAvadani <catalinaavadani@gmail.com>
2024-05-06 15:06:56 +0200

    Fixed brunch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,30 +1,27 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import {
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-} from "react-router-dom";
-import App from "./App";
-import CreateEvent from "./pages/CreateEvent";
 } from 'react-router-dom';
 import App from './App';
+import CreateEvent from './pages/CreateEvent';
 import EventPage from './pages/EventPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
-
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" Component={App}>
+    <Route path='/' Component={App}>
       <Route index Component={HomePage} />
-      <Route path="login" Component={LoginPage} />
-      <Route path="events" Component={EventPage} />
-      <Route path="create" Component={CreateEvent} />
+      <Route path='login' Component={LoginPage} />
+      <Route path='events' Component={EventPage} />
+      <Route path='create' Component={CreateEvent} />
       <Route path='register' Component={RegisterPage} />
       <Route path='*' element={<div>Page not found</div>} />
     </Route>
@@ -33,7 +30,7 @@ const router = createBrowserRouter(
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       {/* <AuthProvider> */}
