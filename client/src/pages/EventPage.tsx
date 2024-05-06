16751709c92cce242f2f24061b62c91cb7b0e75c
CatAvadani <catalinaavadani@gmail.com>
2024-05-06 13:32:10 +0200

    Try uploading images

--- client/src/pages/EventPage.tsx
+++ client/src/pages/EventPage.tsx
@@ -30,11 +30,7 @@ export default function EventPage() {
           {events?.map((event) => (
             <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
               <a href='#'>
-                <img
-                  className='rounded-t-lg'
-                  src='https://images.unsplash.com/photo-1501555088652-021faa106b9b?q=80&w=1746&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                  alt=''
-                />
+                <img className='rounded-t-lg' src={event.image} alt='event' />
               </a>
               <div className='p-5'>
                 <a href='#'>
