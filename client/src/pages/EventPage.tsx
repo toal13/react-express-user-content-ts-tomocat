455d38a429a2bc1b4df72b30e429bb881c603cdb
CatAvadani <catalinaavadani@gmail.com>
2024-05-10 12:25:32 +0200

    Fixed read more

--- client/src/pages/EventPage.tsx
+++ client/src/pages/EventPage.tsx
@@ -58,7 +58,6 @@ export default function EventPage() {
           {events?.map((event) => (
             <div
               key={event.id}
-              onClick={() => event.id && showEventDetails(event.id)}
               className=' max-w-80 md:max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'
             >
               <a href='#'>
@@ -103,6 +102,7 @@ export default function EventPage() {
                     <div>
                       <a
                         href='#'
+                        onClick={() => event.id && showEventDetails(event.id)}
                         className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                       >
                         Read more
